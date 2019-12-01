bca8dbfa370c4c2d2d4aed1d1c07867df0b14804
sheenli <695493298@qq.com>
2019-12-01 22:58:58 +0800

    修改忽略

--- layapro/release/wxgame/index.js
+++
@@ -1,1 +0,0 @@
-window.screenOrientation="sensor_landscape",loadLib("libs/laya.core.js"),loadLib("libs/laya.ui.js"),loadLib("libs/laya.d3.js"),loadLib("libs/domparserinone.js"),loadLib("libs/bytebuffer.js"),loadLib("libs/cannon/cannon.js"),loadLib("js/bundle.js");
